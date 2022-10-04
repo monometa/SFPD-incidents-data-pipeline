a8421278e14436a4f873aff6eef0d6c0a43eb171
monomenta <il.kaltovich@gmail.com>
2022-10-04 20:39:57 +0000

    feat: add script for downloading data from source

---
+++ airflow/dags/scripts/download_datasets.sh
@@ -0,0 +1,5 @@
+# TO-DO: create var and give to curl oper. for downloading diff. datasets 
+
+curl -sSL 192.168.208.1:8000/police_data__2018-2022.csv > $AIRFLOW_HOME/police_data__2018-2022.csv
+
+# curl -sSL 192.168.208.1:8000/police_data__2003-2017.csv > $AIRFLOW_HOME/police_data__2003-2017.csv
