b13f42457f5ca69677f27785f1996d74468a7e1b
monomenta <il.kaltovich@gmail.com>
2022-10-17 13:50:09 +0000

    fix: update file names & sources for FROM clause

--- dbt/models/staging/int_top20_inc_cat.sql
+++ dbt/models/staging/int_top20_inc_cat.sql
@@ -2,7 +2,7 @@ select
     INCIDENT_CATEGORY,
     COUNT(INCIDENT_CATEGORY) as COUNT_OF_INCIDENTS
 
-from {{ source('police_staging', 'sfpd_data_2018_to_present_external_table') }}
+from {{ ref('int_modern') }}
 
 where LOWER(INCIDENT_CATEGORY) not like '%other%'
 group by INCIDENT_CATEGORY
